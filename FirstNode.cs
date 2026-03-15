91ab2e3228ed2df15274b252e4a7271a8b2b6961
Gloria M <69112954+gjmorkos@users.noreply.github.com>
2026-03-14 23:13:40 -0400

    Add files via upload

---
+++ FirstNode.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+[GlobalClass]
+public partial class FirstNode : Node
+{
+	[Export]
+	int health = 100;
+	public override void _Ready()
+	{
+		GD.Print("FirstNode is ready with health: " + health);
+	}
+}
